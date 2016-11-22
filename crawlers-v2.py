e6d4340fec3296dccc1736c60bb1c7ed313e1ed7
Slot009 <slotdeng@163.com>
2016-11-22 20:15:52 +0800

    Add files via upload

---
+++ crawlers-v2.py
@@ -0,0 +1,51 @@
+#/usr/local/bin/python
+
+import re
+import urlparse
+import robotparser
+
+from download import *
+from throttle import Throttle
+
+def link_crawler(seed_url, link_regex, max_depth=2):
+	'''Crawl from the given seed URL following links matched by link_regex.
+	v2.0. Added vioding scraping forever.
+	'''
+
+	crawl_queue = [seed_url]
+	# keep track which URL's have seen before
+	seen = {}
+	rp = robotparser.RobotFileParser()
+
+	while crawl_queue:
+		url = crawl_queue.pop()
+
+		# check url passes robots.txt restrictions
+		if rp.can_fetch(user_agent, url):
+			html = download4(url)
+			# filter for links matching our regular expression
+			for link in get_links(html):
+				if re.match(link_regex, link):
+					# convert relative link to absolute link
+					link = urlparse.urljoin(seed_url, link)
+					crawl_queue.append(link)
+		else:
+			print('Blocked by robots.txt: ', url)
+
+
+def get_links(html):
+	'''Return a list of links from html.
+	'''
+
+	# a regular expression to extract all links from the webpage
+	webpage_regex = re.compile('<a[^>]_href=["\'](.*?)["\']', re.IGNORECASE)
+	# list if all links from the webpage
+	return webpage_regex.findall(html)
+
+
+if __name__ == '__main__':
+	url = 'http://example.webscraping.com'
+	my_regex = '/(index|view)'
+	link_crawler(url, my_regex)
+
+		
